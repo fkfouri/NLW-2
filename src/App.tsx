f85607daf4a66efb692a8fafb2d7ba0bdacf4308
fkfouri <kfouri.fabio@gmail.com>
2020-08-03 15:50:32 -0300

    Criacao da pagina landing

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import Landing from './pages/Landing'
 
+import './assets/styles/global.css';
+
+//um componente/funcao que retorna um html
+// JSX - JavaScript + XML
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+
+    <Landing/>
   );
 }
 
