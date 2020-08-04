e860a011512d13c8f28a0f0c9ca311799370b4ec
fkfouri <kfouri.fabio@gmail.com>
2020-08-04 07:32:34 -0300

    Criaçao do componente TeacherItem

--- src/pages/TearcherList/index.tsx
+++ src/pages/TearcherList/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import PageHeader from '../../components/PageHeader';
-import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './style.css'
+import TeacherItem from '../../components/TeacherItem';
 
 function TeacherList(){
     return (
@@ -25,30 +25,12 @@ function TeacherList(){
             </PageHeader>
 
             <main>
-                <article className="teacher-item">
-                    <header>
-                        <img src="https://lh3.googleusercontent.com/ogw/ADGmqu9_Hcz-dXJCyuQ1EEO8dbslG5_Mtlf4eb0dimGx=s83-c-mo" alt="Fabio Kfouri"/>
-                        <div>
-                            <strong>Fabio Kfouri</strong>
-                            <span>Matemática</span>
-                        </div>
-                    </header>
-                    <p> 
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. <br />
-                        Cras et molestie erat. Vestibulum auctor feugiat varius. Fusce sed tortor imperdiet, efficitur velit a, dapibus eros. Proin mollis dolor ut leo.
-                    </p>
-                    <footer>
-                        <p>
-                            Preço/Hora
-                            <strong>R$ 45,00</strong>
-                        </p>
-                        <button type="button">
-                            <img src={whatsappIcon} alt="Whatsapp"/>
-                            Entrar em contato
-                        </button>
-                    </footer>
-                </article>
-
+                <TeacherItem></TeacherItem>
+                <TeacherItem></TeacherItem>
+                <TeacherItem></TeacherItem>
+                <TeacherItem></TeacherItem>
+                <TeacherItem></TeacherItem>
+                <TeacherItem></TeacherItem>
             </main>
         </div>
     )
