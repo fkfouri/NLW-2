82c44b1dfc16ba0ca35adb30bb55649a37b2561c
fkfouri <kfouri.fabio@gmail.com>
2020-08-05 15:04:25 -0300

    criando um servico simpes

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,12 @@
-console.log('Hello World2')
+import express from 'express'
+
+const app = express();
+
+app.get('/users', (request, response) => {
+    console.log('acessou a rota')
+    return response.send('hello world1')
+    //return response.json('hello world')
+})
+
+//http://localhost:3333
+app.listen(3333);
