c8946bf9026d61d50070a18a1407022854279e2b
fkfouri <kfouri.fabio@gmail.com>
2020-08-06 06:53:31 -0300

    Listagem de aulas com join entre tabela classes e users

---
+++ server/src/controllers/ClassesController.ts
@@ -0,0 +1,99 @@
+import { Request, Response } from 'express'                         //importar o tipo/interface do express
+import db from '../database/connection';
+import convertHoutToMinutes from '../utils/convertHourToMinutes';
+
+
+interface ScheduleItem{
+    week_day: number
+    from: string
+    to: string
+}
+
+export default class ClassesController{
+    //um retorno de lista no padrao MVC costuma se chamar index
+    async index (request: Request, response : Response){
+        const filters = request.query;
+
+        if (!filters.week_day || !filters.subject || !filters.time ){
+            return response.status(400).json({
+                error: 'Missing filter to search classes.'
+            })
+        }
+
+        const subject = filters.subject as string;
+        const week_day = filters.week_day as string;
+        const time = filters.time as string;
+
+        const timeInMinutes = convertHoutToMinutes(time)
+
+        const classes = await db('classes')
+                                .where('classes.subject', '=', subject)
+                                .join('users', 'classes.user_id', '=', 'users.id')
+                                .select(['classes.*', 'users.*'])
+
+        return response.json(classes)
+    }
+
+    //criacao assincrona
+    async create (request : Request, response : Response) {
+        //desestruturacao do json
+        const {
+            name,
+            avatar,
+            whatsapp,
+            bio,
+            subject,
+            cost,
+            schedule
+        } = request.body
+    
+        const trx = await db.transaction()
+    
+        try{
+                
+            //criando o usuario
+            const insertedUserIds = await trx('users').insert(                    //db demora um pouco, entao uso conceito de promisse
+                //usando short sintaxe
+                {name, avatar, whatsapp, bio }
+                // poderia ser assim: { name: name, avatar: avatar, etc}
+                )
+    
+            const user_id = insertedUserIds[0];
+    
+            //criando a classe
+            const insertedClassesIds = await trx('classes').insert({
+                subject, cost, user_id
+            })
+    
+            const class_id = insertedClassesIds[0]
+    
+            //vou percorrer cada item do array
+            const classSchedule_genericType = schedule.map((scheduleItem : any) => { })
+            const classSchedule_usingInterface = schedule.map((scheduleItem : ScheduleItem) => { 
+                return{
+                    class_id,
+                    week_day: scheduleItem.week_day,
+                    from: convertHoutToMinutes(scheduleItem.from),
+                    to: convertHoutToMinutes(scheduleItem.to)
+                }
+            })
+    
+            await trx('class_schedule').insert(classSchedule_usingInterface);
+    
+            //commit a transaction
+            await trx.commit()
+    
+            //codigo de executado com sucesso
+            return response.status(201).send()
+        } catch(err){
+            await trx.rollback()
+    
+            //codigo 400 é um tipo de erro
+            return response.status(400).json({
+                error: 'Unexpected error while creating new class'
+            })
+    
+        }
+       
+    }
+}
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,82 +1,14 @@
 import express, { Router, response } from 'express'
-import db from './database/connection';
-import convertHoutToMinutes from './utils/convertHourToMinutes';
+import ClassesController from './controllers/ClassesController';
 
 const routes = express.Router();
+const classesController = new ClassesController
 
 routes.get('/', (request, response) => {
     return response.json({ message: 'hello world'})
 })
 
-interface ScheduleItem{
-    week_day: number
-    from: string
-    to: string
-}
-
-//transformou em assincrono
-routes.post('/classes', async (request, response) =>{
-    //desestruturacao do json
-    const {
-        name,
-        avatar,
-        whatsapp,
-        bio,
-        subject,
-        cost,
-        schedule
-    } = request.body
-
-    const trx = await db.transaction()
-
-    try{
-            
-        //criando o usuario
-        const insertedUserIds = await trx('users').insert(                    //db demora um pouco, entao uso conceito de promisse
-            //usando short sintaxe
-            {name, avatar, whatsapp, bio }
-            // poderia ser assim: { name: name, avatar: avatar, etc}
-            )
-
-        const user_id = insertedUserIds[0];
-
-        //criando a classe
-        const insertedClassesIds = await trx('classes').insert({
-            subject, cost, user_id
-        })
-
-        const class_id = insertedClassesIds[0]
-
-        //vou percorrer cada item do array
-        const classSchedule_genericType = schedule.map((scheduleItem : any) => { })
-        const classSchedule_usingInterface = schedule.map((scheduleItem : ScheduleItem) => { 
-            return{
-                class_id,
-                week_day: scheduleItem.week_day,
-                from: convertHoutToMinutes(scheduleItem.from),
-                to: convertHoutToMinutes(scheduleItem.to)
-            }
-        })
-
-        await trx('class_schedule').insert(classSchedule_usingInterface);
-
-        //commit a transaction
-        await trx.commit()
-
-        //codigo de executado com sucesso
-        return response.status(201).send()
-    } catch(err){
-        await trx.rollback()
-
-        //codigo 400 é um tipo de erro
-        return response.status(400).json({
-            error: 'Unexpected error while creating new class'
-        })
-
-    }
-
-
-   
-})
+routes.get('/classes', classesController.index)
+routes.post('/classes', classesController.create)
 
 export default routes
