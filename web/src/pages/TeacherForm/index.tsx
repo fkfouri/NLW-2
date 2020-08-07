89af689f1a38f5a5ccb360c3c7b126aa0db6e610
fkfouri <kfouri.fabio@gmail.com>
2020-08-07 05:19:52 -0300

    Definindo o uso de estado do React

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PageHeader from '../../components/PageHeader';
 import Input from '../../components/Input';
 
@@ -10,6 +10,25 @@ import Select from '../../components/Select';
 
 
 function TeacherForm(){
+    /*
+    O React nao fica observando variaveis comuns, somente observa se uma variavel foi criada usando o conceito de estado.
+
+    O useState retorna um vetor (2 coisas), o primeiro sao os itens do calendario e o segundo eh uma funcao que pode substituir o valor do scheduleItems
+    
+    Quando se usa o estado no React, nao se pode fazer modificacoes diretas na variavel devido ao conceito de imutabilidade das variaveis.
+    */
+    const [scheduleItems, setScheduleItems] = useState([
+        {week_day: 0, from: '', to: ''}
+    ])
+
+    function addNewScheduleItem(){
+        setScheduleItems([
+            ...scheduleItems, //spread operator faz uma copia do array original
+            {week_day: 0, from: '', to: ''}
+        ])
+    }
+
+
     return (
         <div id="page-teacher-form" className="container">
             <PageHeader 
@@ -46,27 +65,33 @@ function TeacherForm(){
                 <fieldset>
                     <legend>
                         Horários Disponíveis
-                        <button type="button">
+                        <button type="button" onClick={addNewScheduleItem}>
                             + Novo horário
                         </button>
                     </legend>
                 </fieldset>
-                <div className="schedule-item">
-                    <Select 
-                        name="week-day" 
-                        label="Dia da Semana"
-                        options ={[
-                            {value: '0', label: 'Domingo'},
-                            {value: '1', label: 'Segunda'},
-                            {value: '2', label: 'Terça'},
-                            {value: '3', label: 'Quarta'},
-                            {value: '4', label: 'Quinta'},
-                            {value: '5', label: 'Sexta'},
-                            {value: '6', label: 'Sábado'},
-                        ]} />
-                    <Input name="from" label="Das" type="time" />
-                    <Input name="to" label="Até" type="time" />
-                </div>
+
+                {scheduleItems.map(scheduleItem =>{
+                    return(
+                        <div key={scheduleItem.week_day} className="schedule-item">
+                            <Select 
+                                name="week-day" 
+                                label="Dia da Semana"
+                                options ={[
+                                    {value: '0', label: 'Domingo'},
+                                    {value: '1', label: 'Segunda'},
+                                    {value: '2', label: 'Terça'},
+                                    {value: '3', label: 'Quarta'},
+                                    {value: '4', label: 'Quinta'},
+                                    {value: '5', label: 'Sexta'},
+                                    {value: '6', label: 'Sábado'},
+                                ]} />
+                            <Input name="from" label="Das" type="time" />
+                            <Input name="to" label="Até" type="time" />
+                        </div>
+                    )
+                })}
+
                 <footer>
                     <p>
                         <img src={warningIcon} alt="Aviso importante"/>
